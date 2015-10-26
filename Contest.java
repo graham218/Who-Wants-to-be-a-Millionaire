efd836facf18fb34fa92fb9b2f9da465c7356ed6
Stephen Yang <YangStephen@users.noreply.github.com>
2015-10-25 21:42:59 -0400

    Update Contest.java

--- Contest.java
+++ Contest.java
@@ -1,5 +1,4 @@
 /* 
-Culminating Activity
 Stephen Yang
 Program Name: Contest.java
 Description: This program is the main program of the Who Wants to be a Millionaire game. In this file, it outputs all the questions and possible answers then asks the user for a response. Afterwards, It verifies the answer and calls on the according methods from other classes to do tasks. In the end, It also calculates the winners and outputs them.
